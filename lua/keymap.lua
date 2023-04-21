4f0678adf1bcce8b1906ca28c658da46adc77ff9
shino-kei <shino_kei@asagi.waseda.jp>
2023-04-21 20:09:39 +0900

    add debug option

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -95,7 +95,7 @@ keymap("n", "<Leader>w", ":w<Return>", opts)
 vim.cmd([[
 if executable('atcoder-tools')
   command! AtcoderTest !g++ -std=gnu++17 -Wall -Wextra -Wno-unused-result -O2 main.cpp -I /Users/keisuke/workspace/atcoder/ac-library && atcoder-tools test
-  command! AtcoderDebug !g++ -std=gnu++17 -Wall -Wextra -Wno-unused-result -DLOCAL -O2 main.cpp && atcoder-tools test
+  command! AtcoderDebug !g++ -std=gnu++17 -Wall -Wextra -Wno-unused-result -DLOCAL -O0 -g3 main.cpp && atcoder-tools test
   nnoremap <Leader>at :AtcoderTest<CR>
   endif
   ]])
