fb86830ccf5992984459ccaacbb50ea0a16f8c36
shino-kei <shino_kei@asagi.waseda.jp>
2023-04-21 20:09:50 +0900

    fix neosnippet jump option

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,6 +63,9 @@ return require('packer').startup(function(use)
     vim.cmd([[
     let g:neosnippet#enable_snipmate_compatibility = 1
     let g:neosnippet#snippets_directory='~/.nvim/snippets'
+    imap <C-k> <Plug>(neosnippet_expand_or_jump)
+    smap <C-k> <Plug>(neosnippet_expand_or_jump)
+    xmap <C-k> <Plug>(neosnippet_expand_target)
     ]]) end }
 
     use 'Shougo/neosnippet-snippets'
