2fd250f6605e32c6a01fd4b814ba59ada2e8d259
shino-kei <shino_kei@asagi.waseda.jp>
2023-06-28 20:52:29 +0900

    add nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -202,6 +202,11 @@ return require('packer').startup(function(use)
       }
     end}
 
+    use { 'nvim-treesitter/nvim-treesitter', run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end }
+
     -- others
     use 'cespare/vim-toml'
     use {'taketwo/vim-ros',  config= function()
