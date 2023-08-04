6e9e55978d6329e942a66d64e65d50604313fc72
shino-kei <shino_kei@asagi.waseda.jp>
2023-08-04 17:24:44 +0900

    ensure clangd installed

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -3,40 +3,38 @@ require("mason-lspconfig").setup({
 	ensure_installed = {
 		"pyright",
 		"lua_ls",
+		"clangd",
 	},
 })
 
 require("lspconfig").clangd.setup({})
 require("lspconfig").pyright.setup({})
-require("lspconfig").lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          'vim',
-          'require'
-        },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-
-
-
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
 
 local null_ls = require("null-ls")
 null_ls.setup({
