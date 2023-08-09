3b7c8a132c0fa6edeb29689b44484426f913dcf9
shino-kei <keisuke.shinoda.j7t@soken-labs.co.jp>
2023-08-09 14:12:00 +0900

    change keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -101,7 +101,7 @@ if executable('atcoder-tools')
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "e", vim.diagnostic.open_float)
+-- vim.keymap.set("n", "e", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 vim.keymap.set("n", "q", vim.diagnostic.setloclist)
