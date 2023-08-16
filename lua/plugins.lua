843f19019b26940eaf27d0c5fd5f195ab943bf72
shino-kei <shino_kei@asagi.waseda.jp>
2023-08-16 12:54:39 +0900

    add vim-eft

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -229,6 +229,19 @@ return require("packer").startup(function(use)
     end,
   })
 
+  use({
+    "hrsh7th/vim-eft",
+    config = function()
+      vim.cmd([[
+      nmap f <Plug>(eft-f)
+      xmap f <Plug>(eft-f)
+      omap f <Plug>(eft-f)
+      nmap F <Plug>(eft-F)
+      xmap F <Plug>(eft-F)
+      omap F <Plug>(eft-F) ]])
+    end,
+  })
+
   -- appearence
   use("osyo-manga/vim-precious")
   use("Shougo/context_filetype.vim")
