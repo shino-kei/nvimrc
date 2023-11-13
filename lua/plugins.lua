14bba7843cab08cfa1bdeb220cb21905d61a289b
shino-kei <keisuke.shinoda.j7t@soken-labs.co.jp>
2023-11-13 15:51:55 +0900

    +レジスタにヤンクしたときに、OSC52でクリップボードを有効化

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -339,6 +339,25 @@ return require("packer").startup(function(use)
 
   use({ "tyru/capture.vim" })
 
+  use({
+    "ojroques/nvim-osc52",
+    config = function()
+      require("osc52").setup({
+        max_length = 0,                   -- Maximum length of selection (0 for no limit)
+        silent = false,                   -- Disable message on successful copy
+        trim = false,                     -- Trim surrounding whitespaces before copy
+        tmux_passthrough = false,         -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+      })
+      function copy()
+        if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+          require("osc52").copy_register("+")
+        end
+      end
+
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+    end,
+  })
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
