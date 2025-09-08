3715b952edb4726a7434c18a82237c61d6f87d92
KeisukeShinoda <keisuke.shinoda.j7t@soken-labs.co.jp>
2025-09-08 18:14:51 +0900

    osc52を利用したペーストを無効化

--- lua/options.lua
+++ lua/options.lua
@@ -58,10 +58,10 @@ vim.g.clipboard = {
     ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
     ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
   },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
+  -- paste = {
+  --   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+  --   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  -- },
 }
 
 vim.cmd([[nnoremap + <Cmd>let @+ = @@<CR>]])
