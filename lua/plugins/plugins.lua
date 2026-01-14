9658ff8fc6632bfd6e5ac9819e054cca06464435
Keisuke Shinoda <keisuke.shinoda.j7t@soken-labs.co.jp>
2026-01-14 10:57:09 +0900

    nvim-treesitterの仕様変更に対応

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -264,12 +264,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "c", "lua", "rust" },
-        highlight = { enable = true },
-      }
-    end,
+    opts = {
+      ensure_installed = { "c", "lua", "rust" },
+      highlight = { enable = true },
+    },
   },
   {
     "lukas-reineke/indent-blankline.nvim",
